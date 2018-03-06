fab54a0f0d334a3cc24ff9d47445ffd6804b5c1f
Manuel Guarie <manuel.guarie@gmail.com>
2018-03-06 09:01:41 -0300

    Revision 1
    Primer version

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"><div class="logo"></div></a>
+
+       <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul class="navbar-nav  justify-content-end">
+            <li class="nav-item active"><a class="nav-link" href="#">Iniciar Sesi&oacute;n</a></li>
+            <li class="nav-item active"><a class="nav-link" href="#">Crear cuenta</a></li>
+            <li class="nav-item active"><a class="btn btn-primary btn-borde" href="#" role="button">Dar clases particulares</a></li>
+          </ul>
+       </div>
+    </nav>
+    <div class="container">
+      <div class="row justify-content-start">
+        <div class="col-12 centrado txtcentral">
+          <h1>Encontr&aacute; lo que quer&eacute;s aprender</h1>
+          <br>  
+          <div class="row">
+            <div class="col-5">
+              <input class="form-control form-control-lg" type="text" placeholder="Qu&eacute; quer&eacute;s aprender?">
+            </div>
+            <div class="col-3">
+              <input class="form-control form-control-lg" type="text" placeholder="D&oacute;nde?">
+            </div>
+            <div class="col-2">
+              <button type="button" class="btn btn-primary btn-lg">Buscar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br><br>
+      <div class="row justify-content-start">
+        <div class="col-12 centrado">
+          Descargar la App Gr6aacute;tis
+        </div>
+        <div class="col-6">
+          Apple
+        </div>
+        <div class="col-6">
+          Android
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
